5ebc0488d7ef5ad8fedf8baf5f857672e58e9672
JuanSanchezKZ <juanzete@gmail.com>
2023-07-24 11:09:32 -0300

    agregada call a la api

--- src/components/Clima/Clima.jsx
+++ src/components/Clima/Clima.jsx
@@ -1,7 +1,22 @@
-import "./Clima.css"
+
+import axios from 'axios'
+import Banner from '../banner/Banner'
+
+
+
 
 const Clima = () => {
 
+const apiKey = 'a618aa639b910e409586e9837a98064d'
+
+ axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Cordoba,AR&APPID=${apiKey}`).then((resp) => console.log(resp.data.main))
+ 
+    return <>
+    
+        <Banner></Banner>
+    
+    
+    </>
 }
 
 export default Clima
---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,15 @@
+
+function Banner() {
+    return (  
+        <div>
+           <div>
+
+           </div>
+           <div>
+            
+           </div>
+        </div>
+    );
+}
+
+export default Banner;
