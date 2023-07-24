81bf8541f507ccaa4b2a6dd9169656cb8d99d32c
Exequiel Bobruk <bobrukfs@gmail.com>
2023-07-24 15:17:45 -0300

    Arreglo de llamada a la api

--- src/components/Clima/Clima.jsx
+++ src/components/Clima/Clima.jsx
@@ -1,42 +1,60 @@
-
-import axios from 'axios'
-import Banner from '../banner/Banner'
+import axios from 'axios';
+import Banner from '../banner/Banner';
 import React, { useEffect, useState } from 'react';
 
-
-
 const Clima = () => {
-    const apiKey = 'a618aa639b910e409586e9837a98064d';
-    
-let res;
-    const [data, setData] = useState([]);
+  const apiKey = 'a618aa639b910e409586e9837a98064d';
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
-        const fetchData = async () => {
-            try {
-              const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Cordoba,AR&APPID=${apiKey}`);
-              const data = await response.data;
-              setData(data);
-          
-            } catch (error) {
-              console.log(error);
-            }
+  const getLocationWeather = () => {
+    return new Promise((resolve, reject) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          function (position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${apiKey}`;
+            resolve(api);
+          },
+          function (error) {
+            reject(error);
+          }
+        );
+      } else {
+        reject(new Error("El navegador no admite geolocalización"));
+      }
+    });
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const api = await getLocationWeather(); 
+        const response = await axios.get(api); 
+        const weatherData = response.data;
+        setData(weatherData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+        setLoading(false);
+      }
     };
-    
-    fetchData();
-}, []);
 
-return (
-  <>
-    <Banner nombre={data.name}/>
-  </>
-);
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <p>Loading weather data...</p>
+      ) : data ? (
+        <Banner nombre={data.name} temperatura={data.main.temp} />
+      ) : (
+        <p>Failed to fetch weather data.</p>
+      )}
+    </>
+  );
 };
 
-
-
- 
- 
-
-
-export default Clima
+export default Clima;
--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,14 +1,26 @@
 
-function Banner({nombre}) {
+function Banner({nombre, temperatura}) {
+    let icono;
+    let tempCelsius = Math.round(temperatura - 273.15);
+
+    if(tempCelsius >= 5 && tempCelsius < 15 ){
+        icono = 'src/assets/sunCloud.png'
+    } else if (tempCelsius >= 15){
+        icono = 'src/assets/sun.png'
+    } else{
+        icono = 'src/assets/cloudy.png'
+    }
+
     return (  
         <div className="banner">
             <div className="banner__container">
-               
-                <p></p>
-                <p></p>
+                <img className="banner__icon"  src={icono} alt="icono de clima" />
+                <p className="banner__temp">13º</p>
+                <p className="banner__name">{nombre}</p>
             </div>
             <div className="banner__container">
-
+                <p className="banner__time">7:00 PM</p>
+                <p className="banner__day">Sunset Time, Monday</p>
             </div>
            
         </div>
